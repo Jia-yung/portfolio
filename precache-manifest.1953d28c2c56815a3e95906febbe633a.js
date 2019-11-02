4dced65ff7f9079f3909839d9e996ae421802f01
Yap Jia Yung <yapjiayung@gmail.com>
2019-11-02 18:04:58 +0800

    Updates

---
+++ precache-manifest.1953d28c2c56815a3e95906febbe633a.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d1ab7294b8387cd12590222cbb37a3b6",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "189ca8c8c2b47015d106",
+    "url": "/portfolio/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "e480fec0a0180ba665e9",
+    "url": "/portfolio/static/css/main.6117e058.chunk.css"
+  },
+  {
+    "revision": "189ca8c8c2b47015d106",
+    "url": "/portfolio/static/js/2.6da40a82.chunk.js"
+  },
+  {
+    "revision": "e480fec0a0180ba665e9",
+    "url": "/portfolio/static/js/main.7821990c.chunk.js"
+  },
+  {
+    "revision": "24e5ebc2d9554d8af64d",
+    "url": "/portfolio/static/js/runtime~main.6d8f4ea7.js"
+  },
+  {
+    "revision": "c1168d0db813ed7d817f97be242a7923",
+    "url": "/portfolio/static/media/buildings.c1168d0d.jpg"
+  },
+  {
+    "revision": "195f350528dabd659450b33869fdd345",
+    "url": "/portfolio/static/media/congkak.195f3505.png"
+  },
+  {
+    "revision": "638c554f214e9c70528d3604b9965b09",
+    "url": "/portfolio/static/media/donationlogo.638c554f.png"
+  },
+  {
+    "revision": "47ccbe84d6423e6e82813fab07e9cc45",
+    "url": "/portfolio/static/media/facerecognition.47ccbe84.png"
+  },
+  {
+    "revision": "fddf3b6bfb574cdf21cb92aa1c037e89",
+    "url": "/portfolio/static/media/linefollower.fddf3b6b.png"
+  },
+  {
+    "revision": "21cc1bf0ff22f54cfe781e0a633b8b73",
+    "url": "/portfolio/static/media/portfolio2.21cc1bf0.png"
+  },
+  {
+    "revision": "1a84055fa4568c6df7d36e0892df6260",
+    "url": "/portfolio/static/media/profilePicCropped.1a84055f.jpg"
+  },
+  {
+    "revision": "57d023109b1c4aec5d9767e4027b4542",
+    "url": "/portfolio/static/media/rsz_1earth.57d02310.jpg"
+  },
+  {
+    "revision": "c230476faea562a8d0dfeb6a9de3d4ff",
+    "url": "/portfolio/static/media/web-developerCropped.c230476f.jpg"
+  }
+]);
