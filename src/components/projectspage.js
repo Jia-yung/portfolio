3abd057fece7476249a95fcfe493d7404bb04610
Yap Jia Yung <yapjiayung@gmail.com>
2021-09-11 20:58:43 +0800

    Update projectspage.js

--- src/components/projectspage.js
+++ src/components/projectspage.js
@@ -27,19 +27,11 @@ class Projects extends Component {
                                             <Col className="col-auto">                                                                              
                                                 <Project 
                                                     projectTitle="Portfolio"
-                                                    projectDescription={["Awebpage about the materials that showcase my skills, qualifications and education."]}
+                                                    projectDescription={["Axxxxwebpage about the materials that showcase my skills, qualifications and education."]}
                                                     projectLink1="https://github.com/Jia-yung/portfolio"
                                                     projectPic={PortLogo}
                                                 /> 
-                                            </Col>  
-                                            <Col className="col-auto">                                                                              
-                                                <Project 
-                                                    projectTitle="Portfolio"
-                                                    projectDescription={["A webpage about the materials that showcase my skills, qualifications and education."]}
-                                                    projectLink1="https://github.com/Jia-yung/portfolio"
-                                                    projectPic={PortLogo}
-                                                /> 
-                                            </Col>                                                                                            
+                                            </Col>                                                                                         
                                         </Row> 
                                     </Container>                                                          
                                 </Tab>
