59a45807e9e5276dc15b1730739b256997821e32
Yap Jia Yung <yapjiayung@gmail.com>
2021-09-13 23:54:14 +0800

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.a60cc519299fc39fffe3d1eccaf7956c.js"
+  "/portfolio/precache-manifest.a6c1b44c802638f621cde85057f0c8ff.js"
 );
 
 self.addEventListener('message', (event) => {
